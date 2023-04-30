68657144d9b0e64cc1738cf4140eab9189693147
iibrahim70 <aviknuhash70@gmail.com>
2023-04-30 10:23:30 +0600

    implemented the header and nav component

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDn5gjs3sGeYTU_l2FyYCYmnY3kk_WgdCs",
+  authDomain: "the-news-dragon-88d7c.firebaseapp.com",
+  projectId: "the-news-dragon-88d7c",
+  storageBucket: "the-news-dragon-88d7c.appspot.com",
+  messagingSenderId: "700798485700",
+  appId: "1:700798485700:web:bf9becb7e49d2e953e7591"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
