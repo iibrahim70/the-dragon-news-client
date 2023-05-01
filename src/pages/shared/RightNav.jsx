81022a66158d7c8e7bb4836528b5b9cffe4caf81
iibrahim70 <aviknuhash70@gmail.com>
2023-05-01 12:09:55 +0600

    added left and right nev

--- src/pages/shared/RightNav.jsx
+++ src/pages/shared/RightNav.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import QZone from './QZone';
 
 const RightNav = () => {
   return (
     <div>
-      <h2>right nav</h2>
+      <h4 className='mt-4'>Login With</h4>
+      <Button className='mb-2' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
+      <Button variant="outline-secondary"><FaGithub /> Login with Github</Button>
+      <div>
+        <h4 className='mt-4'>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item><FaFacebook/>Facebook</ListGroup.Item>
+          <ListGroup.Item><FaTwitter />Twitter</ListGroup.Item>
+          <ListGroup.Item><FaInstagram />Instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
     </div>
   );
 };
